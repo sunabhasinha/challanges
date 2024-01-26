289a9bd3dbbb97939ebfd9e40a9b2522b0012dfe
sunabhasinha <mailsunabha@gmail.com>
2024-01-27 02:43:21 +0530

    added dice roll ui

---
+++ mini-challanges/dice-roll/index.js
@@ -0,0 +1,12 @@
+const one = document.getElementById('one');
+const two = document.getElementById('two');
+const three = document.getElementById('three');
+const four = document.getElementById('four');
+const five = document.getElementById('five');
+const six = document.getElementById('six');
+const seven = document.getElementById('seven');
+const button = document.getElementById('btn');
+
+function handleClick() {}
+
+button.addEventListener('click', handleClick);
